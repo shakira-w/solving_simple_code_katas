984f09506a304d16564a514ffa880428be4c6c3c
Shakira <shellypegs@gmail.com>
2026-02-20 11:17:06 +0000

    exercise two

--- exercise2.py
+++ exercise2.py
@@ -9,9 +9,15 @@
 # Side effects
 #    - None
 def count_characters(text, character):
-    pass
+    txt = text
+    x = txt.count(character)
+    return x
 
 print(count_characters("Its a fantastic day", "a"))
+print(count_characters("Feeling very confident", "e"))
+print(count_characters("Testing my coding", "g"))
 
 # Tasks 
-# - 
+# - read through text string
+# - for each instance of character count
+# - count is (+ 1) (maybe for loop or variable)
